6e118c630dfc862050b9952e0da2560407e5d450
Bogdan Vlad <bogdancristianvlad@yahoo.com>
2017-12-08 14:33:56 +0200

    Added Location entity. Created LocationDetails Activity with a Google Map and a SeekBar. Created a dummy list of locations in LocationActivity. Made the connection when pressing on a Location, to be redirected to the location's details. A circle can be drawn on the map in LocationDetails.

---
+++ app/src/main/java/com/chs/app/LocationDetailsActivity.java
@@ -0,0 +1,115 @@
+package com.chs.app;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import com.chs.app.entities.Location;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class LocationDetailsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private SeekBar seekBar;
+    private Location location;
+
+    private final int LOCATION_REQ_PERMISSION = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_details);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        location = this.getIntent().getExtras().getParcelable("Location");
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        location.setMap(mMap);  //map must be set before any other operations with the location
+        mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        if (checkLocationPermission())
+            mMap.setMyLocationEnabled(true);
+        else
+            askLocationPermission();
+
+        location.setMarker(this);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(location.getLatlng()));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+        seekBar = findViewById(R.id.seekBar);
+        location.setCircle(0);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                location.setCircle(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    // Check for permission to access Location
+    private boolean checkLocationPermission() {
+        // Ask for permission if it wasn't granted yet
+        return (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED);
+    }
+
+    // Asks for permission
+    private void askLocationPermission() {
+        ActivityCompat.requestPermissions(
+                this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                LOCATION_REQ_PERMISSION
+        );
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case LOCATION_REQ_PERMISSION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission granted
+                    if (checkLocationPermission())
+                        mMap.setMyLocationEnabled(true);
+                    startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                } else {
+                    Toast.makeText(this, "Application requires location permissions", Toast.LENGTH_LONG);
+                }
+                break;
+            }
+        }
+    }
+}
